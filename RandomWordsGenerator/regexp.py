bc4c27de4f9eb5c9fcb4aad2125da0e06572ebc9
aKomlieva <a.o.komleva@gmail.com>
2016-05-11 23:44:32 +0300

    Update regexp.py

--- RandomWordsGenerator/regexp.py
+++ RandomWordsGenerator/regexp.py
@@ -1,57 +1,81 @@
+# It finally works!
+# Though still only for numbers
+# And there's one thing I don't get:
+# Check-prints shows that it works just fine
+# But final print(object) returns empty string
+
 import random
 import re
 
-signs = re.compile("\+ | \* | \?")
+#signs = re.compile("\+ | \* | \?")
 REG = "-?[0-9]*5+68" # [0 - 9]
-o = ""
+object = ""
 
-def fill(RE, i, o):
+def fill(RE, i, object):
     while i < RE.__len__():
         if re.match("\(", RE[i]):
             j = i + 1
             while RE[j] != ')':
                 j += 1
-            str = RE[i+1:j]
-            if re.match(signs, RE[j+1]) is None:
-                fill(str, 0)
-            else:
-                if RE[j+1] == '?':
-                    N = random.randint(0, 1)
-                elif RE[j+1] == '*':
-                    N = random.randint(0, 10)
+            substring = RE[i+1:j]
+            if j+1 < RE.__len__():
+                if re.match('\?', RE[j+1]):
+                    amount = random.randint(0, 1)
+                elif re.match('\*', RE[j+1]):
+                    amount = random.randint(0, 10)
+                elif re.match('\+', RE[j+1]):
+                    amount = random.randint(1, 10)
                 else:
-                    N = random.randint(1, 10)
-                for k in range(N):
-                    fill(str, 0)
+                    amount = 1
+                for k in range(amount):
+                    fill(substring, 0, object)
+            else:
+                fill(substring, 0, object)
         elif re.match("\[", RE[i]):
-            if re.match(signs, RE[i+5]) is None:
-                X = random.randint(int(RE[i + 1]), int(RE[i + 3]))
-                o += X
+            if i+5 < RE.__len__():
+                flag = 0
+                if re.match('\?', RE[i + 5]):
+                    amount = random.randint(0, 1)
+                elif re.match('\*', RE[i + 5]):
+                    amount = random.randint(0, 10)
+                elif re.match('\+', RE[i + 5]):
+                    amount = random.randint(1, 10)
+                else:
+                    flag = 1
+                    amount = 1
+                for k in range(amount):
+                    object += (random.randint(int(RE[i + 1]), int(RE[i + 3]))).__str__()
+                    print("branch ", object)
+                if flag == 0:
+                    i += 6
+                else:
+                    i += 5
+            else:
+                object += (random.randint(int(RE[i + 1]), int(RE[i + 3]))).__str__()
+                print("branch ", object)
                 i += 5
-            else:
-                if RE[i+5] == '?':
-                    N = random.randint(0, 1)
-                elif RE[i+5] == '*':
-                    N = random.randint(0, 10)
-                else:
-                    N = random.randint(1, 10)
-                for k in range(N):
-                    X = random.randint(int(RE[i + 1]), int(RE[i + 3]))
-                    o += X
-                i += 6
         else:
-            if re.match(signs, RE[i+1]) is None:
-                o += RE[i]
-                i += 1
-            else:
-                if RE[i+1] == '?':
-                    N = random.randint(0, 1)
-                elif RE[i+1] == '*':
-                    N = random.randint(0, 10)
+            if i+1 < RE.__len__():
+                flag = 0
+                if re.match('\?', RE[i + 1]):
+                    amount = random.randint(0, 1)
+                elif re.match('\*', RE[i + 1]):
+                    amount = random.randint(0, 10)
+                elif re.match('\+', RE[i + 1]):
+                    amount = random.randint(1, 10)
                 else:
-                    N = random.randint(1, 10)
-                for k in range(N):
-                    o += RE[i]
-                i += 2
-fill(REG, 0, o)
-print(o)
+                    flag = 1
+                    amount = 1
+                for k in range(amount):
+                    object += RE[i]
+                    print("single ", object)
+                if flag == 0:
+                    i += 2
+                else:
+                    i += 1
+            else:
+                object += RE[i]
+                print("single ", object)
+                i += 1
+fill(REG, 0, object)
+print(object)
