c48ce3d52b0164c923f14d596bac692bed36a25f
aKomlieva <a.o.komleva@gmail.com>
2016-05-11 21:25:58 +0300

    Update regexp.py

--- RandomWordsGenerator/regexp.py
+++ RandomWordsGenerator/regexp.py
@@ -1,42 +1,57 @@
-# Bad news:
-# It still needs a lot of optimization (fortunately I know how to manage that)
-# Now it works only for arithmetical expressions
-# Part with braces (first if) isn't finished yet, that part requires some recursion
-
 import random
 import re
 
-RE = "-?[0 - 9][a-z][0-9]"
 signs = re.compile("\+ | \* | \?")
-o = "" # o for object, it's our return
-i = 0
-while i < RE.__len__():
-    if re.match("\(", RE[i]):
-        j = i + 1
-        while RE[j] != ')':
-            j += 1
-    elif re.match("\[", RE[i]):
-        X = random.randint(RE[i+1], RE[i+3])
-        if re.match(signs, RE[i+5]) is None:
-            o += X
-        else:
-            if RE[i+5] == '?':
-                N = random.randint(0, 1)
-            elif RE[i+5] == '*':
-                N = random.randint(0, 10)
+REG = "-?[0-9]*5+68" # [0 - 9]
+o = ""
+
+def fill(RE, i, o):
+    while i < RE.__len__():
+        if re.match("\(", RE[i]):
+            j = i + 1
+            while RE[j] != ')':
+                j += 1
+            str = RE[i+1:j]
+            if re.match(signs, RE[j+1]) is None:
+                fill(str, 0)
             else:
-                N = random.randint(1, 10)
-            for k in range(N):
+                if RE[j+1] == '?':
+                    N = random.randint(0, 1)
+                elif RE[j+1] == '*':
+                    N = random.randint(0, 10)
+                else:
+                    N = random.randint(1, 10)
+                for k in range(N):
+                    fill(str, 0)
+        elif re.match("\[", RE[i]):
+            if re.match(signs, RE[i+5]) is None:
+                X = random.randint(int(RE[i + 1]), int(RE[i + 3]))
                 o += X
-    else:
-        if re.match(signs, RE[i+1]) is None:
-            o += RE[i]
-        else:
-            if RE[i+1] == '?':
-                N = random.randint(0, 1)
-            elif RE[i+1] == '*':
-                N = random.randint(0, 10)
+                i += 5
             else:
-                N = random.randint(1, 10)
-            for k in range(N):
+                if RE[i+5] == '?':
+                    N = random.randint(0, 1)
+                elif RE[i+5] == '*':
+                    N = random.randint(0, 10)
+                else:
+                    N = random.randint(1, 10)
+                for k in range(N):
+                    X = random.randint(int(RE[i + 1]), int(RE[i + 3]))
+                    o += X
+                i += 6
+        else:
+            if re.match(signs, RE[i+1]) is None:
                 o += RE[i]
+                i += 1
+            else:
+                if RE[i+1] == '?':
+                    N = random.randint(0, 1)
+                elif RE[i+1] == '*':
+                    N = random.randint(0, 10)
+                else:
+                    N = random.randint(1, 10)
+                for k in range(N):
+                    o += RE[i]
+                i += 2
+fill(REG, 0, o)
+print(o)
